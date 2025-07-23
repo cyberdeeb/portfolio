adc83ffb7dd96a0ec0a37bde6bdcbe783d0e5b4e
cyberdeeb <abrahamdeeb@gmail.com>
2025-07-23 11:17:16 -0700

    Connect tailwind and build out loading page

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import {Loading} from "./components/Loading"
+import "./index.css"
 
 function App() {
-  
+  const [isLoaded, setIsLoaded] = useState(false)
 
   return (
     <>
-     
+     {!isLoaded && <Loading onComplete={() => setIsLoaded(true)}/>}
     </>
   )
 }
